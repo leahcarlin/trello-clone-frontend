d0bf00e3d1a41ecda7dbdd7dd7a82bc3e08ec45c
leahcarlin <leahrcarlin@gmail.com>
2021-12-20 07:40:57 +0100

    Add list input updates lists array and maps to App page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import { useState } from "react";
 //components
 import Welcome from "./components/Welcome";
 
+//styles
+import { Header } from "./App.styles";
+
 function App() {
   const [boardName, setBoardName] = useState("");
   const [name, setName] = useState("");
+  const [listName, setListName] = useState("");
+  const [lists, setLists] = useState([]);
 
   // create new board and store name of board
   const createBoard = (e) => {
@@ -13,6 +18,15 @@ function App() {
     setName(boardName);
     setBoardName("");
   };
+
+  // create a new list (column)
+  const createList = (e) => {
+    e.preventDefault();
+    setLists([...lists, { title: listName, cards: [] }]);
+    setListName("");
+  };
+
+  console.log("lists?", lists);
 
   return (
     <div className="App">
@@ -23,8 +37,25 @@ function App() {
           createBoard={createBoard}
         />
       ) : (
-        <h2>{name}</h2>
+        <Header>
+          <h3>{name}</h3>
+          <form onSubmit={createList}>
+            <input
+              value={listName}
+              onChange={(e) => setListName(e.target.value)}
+              type="test"
+              id="listName"
+              placeholder="Add a list..."
+            ></input>
+            <button type="submit">Add</button>
+          </form>
+        </Header>
       )}
+      <div>
+        {lists.map((list) => (
+          <p>{list.title}</p>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/App.styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Header = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 20px 10px;
+
+  h3 {
+    margin-right: 10px;
+  }
+`;
